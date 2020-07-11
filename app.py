592f3aedbac9fb39c2705637cc2c65f62f0b8f76
liam-nguyen <nguyen.dch@gmail.com>
2020-07-11 17:05:04 -0500

    Add app.py and static index.html

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from flask_jwt_extended import JWTManager
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+
+from resources.item import Item, ItemList
+from resources.store import Store, StoreList
+from resources.user import UserRegistration
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'jose'  # JWT Secret Key
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Change the extension behavior but not the underlying behaviors
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+
+api = Api(app)
+jwt = JWTManager(app)
+db = SQLAlchemy(app)
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+api.add_resource(UserRegistration, '/register')
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(StoreList, '/stores')
+
+
+@app.before_first_request
+def create_table():
+    db.create_all()
+
+
+if __name__ == "__main__":
+    from db import db
+
+    db.init_app(app)
+    app.run(port=5000, debug=True)
